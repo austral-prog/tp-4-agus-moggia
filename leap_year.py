b2a5d1142ea6306837df3d62ab6e95dc4e0b0976
agus-moggia <amoggia@mail.austral.edu.ar>
2025-04-14 16:14:08 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,11 @@
 def leap_year():
-   A = int(input("Ingrese un año: "))
-
-    if A%4 == 0:
+	A = int(input("Ingrese un año: "))
+	if A%4 == 0:
 	    if A%100 != 0:
-		    print(f"El año {A} es biciesto")
-	    if A%400 == 0:
-		    print(f"El año {A} es biciesto")
-    else:
-	    print(f"El año {A} no es biciesto")
+		    print(f"El año {A} es bisiesto")
+	    elif A%400 == 0:
+		    print(f"El año {A} es bisiesto")
+		else:
+			print(f"El año {A} no es bisiesto")
+	else:
+		print(f"El año {A} no es bisiesto")
