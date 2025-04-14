c2eb7f73b624bb2c26d0a7772d5d5ea8967356d6
agus-moggia <amoggia@mail.austral.edu.ar>
2025-04-14 16:16:57 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,8 +4,8 @@ def leap_year():
 	    if A%100 != 0:
 		    print(f"El año {A} es bisiesto")
 	    elif A%400 == 0:
-		    print(f"El año {A} es bisiesto")
-		else:
-			print(f"El año {A} no es bisiesto")
+	    	print(f"El año {A} es bisiesto")
+	    else:
+	    	print(f"El año {A} no es bisiesto")
 	else:
 		print(f"El año {A} no es bisiesto")
